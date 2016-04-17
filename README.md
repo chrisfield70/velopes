1ce215756c13447e2a60fd9cba5766944103b99d
Chris Field <chrisfield@Chriss-MacBook-Pro.local>
2016-04-17 14:32:47 +0100

    Up and Running with Python and Django
    some messing, then implementing the Lynda.com course.

--- README.md
+++ README.md
@@ -25,3 +25,6 @@ Pretty cool.
 
 
 
+
+
+
---
+++ firstdjango/inventory/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import Http404
+
+from inventory.models import Item
+
+# from django.http import HttpResponse
+
+
+def index(request):
+	items = Item.objects.exclude(amount=0)
+	return render(request, 'inventory/index.html', {
+		'items': items,
+		})
+
+	# return HttpResponse('<p>In index view</p>')
+
+
+def item_detail(request, id):
+	try:
+		item = Item.objects.get(id=id)
+	except Item.DoesNotExist:
+		raise Http404('This item does not exist')
+	return render(request, 'inventory/item_detail.html', {
+		'item':item,
+		})
+
+
+
+
